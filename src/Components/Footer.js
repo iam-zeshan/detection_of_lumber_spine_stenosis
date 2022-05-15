e0de59a824e59a5f9f691309b4fcee42199a32b6
Zeshan Haider <zeshan.haider.turi@gmail.com>
2022-05-15 12:30:54 -0600

    done some changes

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div>
+            <div className="container-fluid p-5 my-5 bg-dark text-white" style={{backgroundColor:"black", color:"white", height:200, marginBottom:-30}}>
+            <section className="copyright text-center text-white" style={{marginTop:100}}>
+                <div className="container">
+                    <small >Copyright &copy; All Rights Reserved developed by Ali Ullah, Khalid Khan & Zeshan Haider</small>
+                </div>
+            </section>
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <Link className="navbar-brand" to="/">SARFUI</Link>
+                    </div>
+                    <ul className="nav navbar-nav">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/team">Team</Link></li>
+                        <li><Link to="/contact">Contact Us</Link></li>
+                    </ul>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><Link to="/signup"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
+                        <li><Link to="/signin"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
