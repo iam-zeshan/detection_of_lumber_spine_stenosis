82e6e252b69782058bcaa7c03e50ea9a2b83674a
Zeshan Haider <zeshan.haider.turi@gmail.com>
2022-05-15 11:54:30 -0600

    I created React app using 'npx create-react-app detection_of_lumber_spine_stenosis'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        detection_of_lumber_spine_stenosis
         </p>
         <a
           className="App-link"
