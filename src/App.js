9c7543119db6349590ce27c4e2dd28e3fe002bc6
Zeshan Haider <zeshan.haider.turi@gmail.com>
2022-05-15 12:30:12 -0600

    I've just made some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        detection_of_lumber_spine_stenosis
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+         
+        </Routes>
+        <Footer />
+      </BrowserRouter> 
+    </>
   );
 }
 
